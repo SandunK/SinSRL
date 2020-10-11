64f2e6810362e08a2203b186060b6f76216b1fee
sandunK <gmgsk.16@cse.mrt.ac.lk>
2020-10-11 15:48:42 +0530

    [BUGFIX] Annotator bean creation error

--- projection/src/main/java/com/dcs/semantic/projection/process/Annotator.java
+++ projection/src/main/java/com/dcs/semantic/projection/process/Annotator.java
@@ -5,7 +5,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Table;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -15,7 +15,7 @@ import java.util.Map;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
-
+@Controller
 public class Annotator {
     private static final Logger logger = LogManager.getLogger(Annotator.class);
     private static LanguageDAO languageDAO = LanguageDAO.getInstance();
